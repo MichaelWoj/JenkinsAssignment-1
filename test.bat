656038c5b26821804e16fd66301cf6522d14a703
conleth <conlethkennedy@gmail.com>
2018-12-31 14:12:25 +0000

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,4 +1,4 @@
 rem javac studentTest.java 
 rem java studentTest.java
-javac -cp junit-4.13-beta-1.jar:Student.java:. studentTest.java
-java -cp junit-4.13-beta-1.jar:hamcrest-core-2.1.jar:. org.junit.runner.JUnitCore studentTest
+javac -cp junit-4.13-beta-1.jar;Student.java;. studentTest.java
+java -cp junit-4.13-beta-1.jar;hamcrest-core-2.1.jar;. org.junit.runner.JUnitCore studentTest
